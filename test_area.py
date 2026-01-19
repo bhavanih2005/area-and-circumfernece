47d0acc17bc923f287021456c2e3b2a209e6f43f
bhavanih2005 <bhavanih9353@gmail.com>
2026-01-19 11:32:44 +0530

    Update test_area.py

--- test_area.py
+++ test_area.py
@@ -2,4 +2,4 @@ from area import area_circumference
 import math
 
 def test_area():
-    assert area_circumference(3) == "area: 28.27, \ncircumference: 18.84"
+    assert area_circumference(3) == "radius :3\n,area: 28.27, \ncircumference: 18.84"
